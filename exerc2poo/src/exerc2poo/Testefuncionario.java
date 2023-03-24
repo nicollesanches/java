4efc77fa7289c49f28004f0c17f9b66d87fd7963
Nicolle Sanches <nicollesanches.ri@outlook.com>
2023-03-24 20:55:29 -0300

    exerc1poo

--- exerc2poo/src/exerc2poo/Testefuncionario.java
+++ exerc2poo/src/exerc2poo/Testefuncionario.java
@@ -7,7 +7,9 @@ public class Testefuncionario {
 
 		
 		Funcionario dados = new Funcionario("Nicolle Sanches", "nicollesanches.ri@outlook.com", "Desenvolvedora Jr", 4000.00f , true );
+		Funcionario dados2 = new Funcionario("Kaun Manoel", "kauanmanoel@outlook.com", "Desenvolvedor Jr", 4000.00f , true );
 		
+		dados.visualizar(); 
 		dados.visualizar(); 
 		
 	}
